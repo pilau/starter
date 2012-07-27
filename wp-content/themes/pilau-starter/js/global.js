7e140fc380041d05a2e63fc517524775d8bcb27d
Steve Taylor <steve@sltaylor.co.uk>
2012-07-27 18:36:21 +0100

    Added JS placeholder fallbackl

--- wp-content/themes/pilau-starter/js/global.js
+++ wp-content/themes/pilau-starter/js/global.js
@@ -41,6 +41,42 @@ jQuery( document ).ready( function( $ ) {
 		$( 'a.make-link-target' ).click( function(e) { e.preventDefault(); });
 	}
 
+	/**
+	 * Placeholder fallback
+	 * @link	http://uniquemethod.com/html5-placeholder-text-jquery-fallback-with-modernizr
+	 */
+	if ( ! Modernizr.input.placeholder ) {
+
+		// set placeholder values
+		$( 'body' ).find( '[placeholder]' ).each( function() {
+			if ( $( this ).val() == '' ) {
+				$( this ).val( $( this ).attr( 'placeholder' ) );
+			}
+		});
+
+		// focus and blur of placeholders
+		$( '[placeholder]' ).on( 'focus', function() {
+			if ( $( this ).val() == $( this ).attr( 'placeholder' ) ) {
+				$( this ).val('');
+				$( this ).removeClass( 'placeholder' );
+			}
+		}).on( 'blur', function() {
+				if ( $( this ).val() == '' || $( this ).val() == $( this ).attr( 'placeholder' ) ) {
+					$( this ).val( $( this ).attr( 'placeholder' ) );
+					$( this ).addClass( 'placeholder' );
+				}
+			});
+
+		// remove placeholders on submit
+		$( '[placeholder]' ).closest( 'form' ).on( 'submit', function() {
+			$( this ).find( '[placeholder]' ).each( function() {
+				if ( $( this ).val() == $( this ).attr( 'placeholder' ) ) {
+					$( this ).val('');
+				}
+			})
+		});
+
+	}
 
 });
 
