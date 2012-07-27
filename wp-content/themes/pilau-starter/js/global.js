2c366b412b41a40189d9086a281ac81162b9fc49
Steve Taylor <steve@sltaylor.co.uk>
2012-07-27 18:42:26 +0100

    Removed make-link JS - use HTML5 anchors wrapping block-level elements instead!

--- wp-content/themes/pilau-starter/js/global.js
+++ wp-content/themes/pilau-starter/js/global.js
@@ -11,36 +11,6 @@ jQuery( document ).ready( function( $ ) {
 	$( '.hide-if-js' ).remove();
 	$( '.hide-if-no-js' ).show();
 
-
-	/** Make containing elements into links */
-	if ( $( '.make-link' ).length ) {
-		$( '.make-link' ).css( 'cursor', 'pointer' ).hover(
-			function() {
-				window.status = $( this ).find( 'a.make-link-target' ).attr( 'href' );
-				$( this ).find( 'a.make-link-target' ).addClass( 'hover' );
-			},
-			function() {
-				window.status = '';
-				$( this ).find( 'a.make-link-target' ).removeClass( 'hover' );
-			}
-		).click( function() {
-			window.location = $( this ).find( 'a.make-link-target' ).attr( 'href' );
-		});
-	}
-	/** Make sibling elements into links */
-	if ( $( '.make-sibling-link' ).length ) {
-		$( '.make-sibling-link' ).css( 'cursor', 'pointer' ).hover(
-			function() { window.status = $( this ).parents( '.make-link-container' ).find( 'a.make-link-target' ).attr( 'href' ); },
-			function() { window.status = ''; }
-		).click( function() {
-			window.location = $( this ).parents( '.make-link-container' ).find( 'a.make-link-target' ).attr( 'href' );
-		});
-	}
-	// Stuff for both types of make-link
-	if ( $( '.make-link, .make-sibling-link' ).length ) {
-		$( 'a.make-link-target' ).click( function(e) { e.preventDefault(); });
-	}
-
 	/**
 	 * Placeholder fallback
 	 * @link	http://uniquemethod.com/html5-placeholder-text-jquery-fallback-with-modernizr
