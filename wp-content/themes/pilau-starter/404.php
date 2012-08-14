afa8fc89bfb4900a3c0cddb91b337f2bc0b7fc44
Steve Taylor <steve@sltaylor.co.uk>
2012-08-14 20:16:36 +0100

    Tweaks to 404.php

--- wp-content/themes/pilau-starter/404.php
+++ wp-content/themes/pilau-starter/404.php
@@ -7,13 +7,21 @@
  * @since	0.1
  * @todo	More useful 404 page?
  */
+global $wp_query;
 
 /**
  * Make sure everything's set so this page can be included from elsewhere
  */
-header( 'HTTP/1.1 404 Not Found' );
-header( 'Status: 404 Not Found' );
+$wp_query->is_404 = true;
+$wp_query->is_single = false;
+$wp_query->is_singular = false;
+$wp_query->is_page = false;
+status_header( '404' );
 add_filter( 'wp_title', 'pilau_404_title', 10000, 3 );
+
+// This hack is needed to prevent an infinite loop caused by this plugin's get_closest_page method
+// A "known issue"!
+remove_filter( 'wp_get_nav_menu_items', array( 'l3radyCPTStructureHelper', 'add_active_to_menus_for_CPTs' ), 3, 10 );
 
 ?>
 
