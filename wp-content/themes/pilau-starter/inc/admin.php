8e1b2a7d7e5f3f0a3ed75a39e6e68a961dab8433
Steve Taylor <steve@sltaylor.co.uk>
2012-08-19 15:31:17 +0100

    Inline image size hints

--- wp-content/themes/pilau-starter/inc/admin.php
+++ wp-content/themes/pilau-starter/inc/admin.php
@@ -42,6 +42,9 @@ function pilau_admin_init() {
 	/* Disable captions */
 	//add_filter( 'disable_captions', '__return_true' );
 
+	/* Set up inline hints for image sizes */
+	//add_filter( 'admin_post_thumbnail_html', 'pilau_inline_image_size_featured', 10, 2 );
+
 	/**
 	 * Installed plugins data
 	 *
@@ -51,6 +54,40 @@ function pilau_admin_init() {
 	if ( PILAU_USE_PLUGINS_PAGE )
 		$pilau_wp_plugins = get_plugins();
 
+}
+
+
+/**
+ * Inline image hints for Featured Image box
+ *
+ * @since	Pilau_Starter 0.1
+ * @param	string	$content
+ * @return	string
+ */
+function pilau_inline_image_size_featured( $content ) {
+	$hint = null;
+
+	switch ( get_post_type() ) {
+		case 'investment':
+			$hint = 'Optimum image size: 647 x 461 px. If larger, try to keep similar proportions.';
+			break;
+		case 'page':
+			switch ( get_page_template_slug() ) {
+				case 'page_priority-area.php' :
+					$hint = 'Optimum image size: 472 x 364 px. If larger, try to keep similar proportions.';
+					break;
+				default:
+					if ( get_page_template_slug() == '' )
+						$hint = 'For listing page images, the optimum size is 203 x 161 px.';
+					break;
+			}
+			break;
+	}
+
+	if ( $hint )
+		$content = '<p><i>' . $hint . '</i></p>' . $content;
+
+	return $content;
 }
 
 
