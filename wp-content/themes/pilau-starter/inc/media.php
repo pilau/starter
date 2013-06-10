64a3313e7c92ded42aa3df1f366043cb7720e89c
Steve Taylor <steve@sltaylor.co.uk>
2013-06-10 11:05:31 +0100

    pilau_image_maybe_caption()

--- wp-content/themes/pilau-starter/inc/media.php
+++ wp-content/themes/pilau-starter/inc/media.php
@@ -68,3 +68,57 @@ function pilau_image_attributes( $attr, $attachment ) {
 	unset( $attr['title'] );
 	return $attr;
 }
+
+
+/**
+ * Output an image with optional caption, using <figure> and <figcaption> tags
+ *
+ * @since	Pilau_Starter 0.1
+ * @param	int				$image_id		ID of the image
+ * @param	string			$size			Size of the image; defaults to 'post-thumbnail'
+ * @param	string			$alt			Alternate text for the image; defaults to image alt or post title
+ * @param	array|string	$fig_class		Class(es) for the <figure> tag
+ * @param	string			$fig_id			ID for the <figure> tag
+ * @return	void
+ */
+function pilau_image_maybe_caption( $image_id, $size = 'post-thumbnail', $alt = null, $fig_class = null, $fig_id = null ) {
+
+	// Try to get image
+	if ( ! ctype_digit( $image_id ) )
+		return;
+	$image = get_post( $image_id );
+	if ( ! $image )
+		return;
+
+	// Initialize
+	$fig_class = (array) $fig_class;
+	$image_meta = wp_get_attachment_metadata( $image_id );
+	$image_width = $image_meta['width'];
+	$image_height = $image_meta['height'];
+	if ( array_key_exists( $size, $image_meta['sizes'] ) ) {
+		$image_width = $image_meta['sizes'][ $size ]['width'];
+		$image_height = $image_meta['sizes'][ $size ]['height'];
+	}
+	if ( ! is_null( $alt ) ) {
+		$image_alt = $alt;
+	} else if ( ! $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt' )[0] ) {
+		$image_alt = $image->post_title;
+	}
+
+	// Start output
+	echo '<figure class="' . esc_attr( implode( ' ', $fig_class ) ) . '"';
+	if ( $fig_id )
+		echo ' id="' . esc_attr( $fig_id ) . '"';
+	echo '>';
+
+	// Image
+	echo '<img src="' . pilau_get_image_url( $image_id, $size ) . '" width="' . esc_attr( $image_width ) . '" height="' . esc_attr( $image_height ) . '" alt="' . esc_attr( $image_alt ) . '">';
+
+	// Caption?
+	if ( $image->post_excerpt ) {
+		echo '<figcaption>' . $image->post_excerpt . '</figcaption>';
+	}
+
+	echo '</figure>';
+
+}
