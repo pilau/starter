27dc7f91734159df19295556f6d46410d15ba838
Steve Taylor <steve@sltaylor.co.uk>
2014-02-13 12:05:31 +0000

    Conditional HTML classes changed for IE 9 targetting

--- wp-content/themes/pilau-starter/header.php
+++ wp-content/themes/pilau-starter/header.php
@@ -12,10 +12,11 @@
  * @link http://paulirish.com/2008/conditional-stylesheets-vs-styles-hacks-answer-neither/
  */
 ?><!DOCTYPE html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7 ie6" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9 ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 ie6" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8 ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie10 lt-ie9 ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]><html class="no-js lt-ie10 ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
