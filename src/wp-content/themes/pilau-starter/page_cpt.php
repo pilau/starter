24e2431c91594be146ff0d0180858f580a36f5f6
Steve Taylor <steve@sltaylor.co.uk>
2015-02-11 20:23:17 +0000

    Sample CPT landing page template

---
+++ src/wp-content/themes/pilau-starter/page_cpt.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Template Name: CPT sample page
+ *
+ * @package	Walsingham
+ * @since	0.1
+ */
+
+$pilau_cpts = get_posts( array(
+	'post_type'			=> 'cpt',
+	'posts_per_page'	=> -1,
+	'orderby'			=> 'title',
+	'order'				=> 'ASC',
+));
+
+?>
+
+<?php get_header(); ?>
+
+	<main role="main" id="content">
+		<div class="wrap">
+
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+				<article <?php post_class(); ?> role="article">
+
+					<h1 class="heading-main"><?php the_title(); ?></h1>
+
+					<div class="editor-content">
+						<?php the_content(); ?>
+					</div>
+
+					<ul class="cpts-list">
+						<?php
+						foreach( $pilau_cpts as $pilau_cpt ) {
+							echo '<li class="cpt-item"><a href="' . get_permalink( $pilau_cpt ) . '">' . get_the_title( $pilau_cpt ) . '</a></li>';
+						}
+						?>
+					</ul>
+
+				</article>
+
+			<?php endwhile; endif; ?>
+
+		</div>
+	</main>
+
+<?php get_footer(); ?>
