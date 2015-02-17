16de96e1184cb3bc53df302da6cd283b9e67fef1
Steve Taylor <steve@sltaylor.co.uk>
2015-02-17 22:54:09 +0000

    Default sidebar

--- src/wp-content/themes/pilau-starter/single.php
+++ src/wp-content/themes/pilau-starter/single.php
@@ -14,6 +14,7 @@
 <main role="main" id="content">
 	<div class="wrap">
 
+
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<article <?php post_class(); ?> role="article">
@@ -33,6 +34,12 @@
 			</article>
 
 		<?php endwhile; endif; ?>
+
+
+		<div role="complementary" class="sidebar">
+			<?php dynamic_sidebar( 'default-sidebar' ); ?>
+		</div>
+
 
 	</div>
 </main>
