36e960c1875d811bd28a88a9b5c5f3d957a9eff4
Steve Taylor <steve@sltaylor.co.uk>
2015-04-15 12:38:45 +0100

    Gravity Forms status detection

--- src/wp-content/themes/pilau-starter/inc/forms.php
+++ src/wp-content/themes/pilau-starter/inc/forms.php
@@ -8,10 +8,32 @@
  */
 
 
-add_filter( 'gform_tabindex', 'pilau_gform_tabindexer', 10, 2 );
+add_action( 'wp', 'pilau_gform_status' );
+/**
+ * Detect status of Gravity Forms submissions
+ *
+ * @since	0.1
+ */
+function pilau_gform_status() {
+	global $pilau_gform_submitted, $pilau_gform_valid;
+	$pilau_gform_submitted = null;
+	$pilau_gform_valid = true;
+
+	if ( PILAU_PLUGIN_EXISTS_GRAVITY_FORMS && isset( $_POST['gform_submit'] ) ) {
+		$pilau_gform_submitted = $_POST['gform_submit'];
+		$submissions_array = GFFormDisplay::$submission;
+		$pilau_gform_valid = $submissions_array[ $pilau_gform_submitted ]['is_valid'];
+	}
+
+}
+
+
+//add_filter( 'gform_tabindex', 'pilau_gform_tabindexer', 10, 2 );
 /**
  * Fix Gravity Form Tabindex Conflicts
  * @link	http://gravitywiz.com/fix-gravity-form-tabindex-conflicts/
+ *
+ * Currently deactivated in favour of Gravity Forms WCAG 2.0 plugin
  */
 function pilau_gform_tabindexer( $tab_index, $form = false ) {
 	$starting_index = 1000; // if you need a higher tabindex, update this number
