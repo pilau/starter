3dbfde8fb4d5ef7c0b5fd118b1e2567b145fe260
Steve Taylor <steve@sltaylor.co.uk>
2015-05-10 13:49:39 +0100

    Tabs refinements

--- src/wp-content/themes/pilau-starter/front-page.php
+++ src/wp-content/themes/pilau-starter/front-page.php
@@ -28,7 +28,7 @@
 
 			<div class="tabs" id="tabs-a">
 
-				<h3 class="hide-if-js">Single tab example</h3>
+				<h3 class="tabs-heading hide-if-js">Single tab example</h3>
 
 				<ul class="tablist-1 hide-if-no-js" role="tablist">
 					<li id="tab-a-1" aria-controls="panel-1-1" aria-selected="true" role="tab" tabindex="0">Tab 1</li>
@@ -36,7 +36,7 @@
 
 				<div class="panels">
 					<div id="panel-a-1" role="tabpanel" aria-labelledby="tab-a-1" aria-hidden="false">
-						<h4 class="hide-if-js">Tab 1 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 1 title</h4>
 						<p>Sometimes you end up using the same style with just a single tab...</p>
 					</div>
 				</div>
@@ -45,7 +45,7 @@
 
 			<div class="tabs" id="tabs-b">
 
-				<h3 class="hide-if-js">Double tab example</h3>
+				<h3 class="tabs-heading hide-if-js">Double tab example</h3>
 
 				<ul class="tablist-2 hide-if-no-js" role="tablist">
 					<li id="tab-b-1" aria-controls="panel-b-1" aria-selected="true" role="tab" tabindex="0">Tab 1</li>
@@ -54,20 +54,20 @@
 
 				<div class="panels">
 					<div id="panel-b-1" role="tabpanel" aria-labelledby="tab-b-1" aria-hidden="false">
-						<h4 class="hide-if-js">Tab 1 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 1 title</h4>
 						<p>First panel contents...</p>
 					</div>
 					<div id="panel-b-2" role="tabpanel" aria-labelledby="tab-b-2" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 2 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 2 title</h4>
 						<p>Second panel contents...</p>
 					</div>
 				</div>
 
 			</div>
 
-			<div class="tabs" id="tabs-c">
+			<div class="tabs tabs-expand-for-mobile" id="tabs-c">
 
-				<h3 class="hide-if-js">Triple tab example</h3>
+				<h3 class="tabs-heading hide-if-js">Triple tab example</h3>
 
 				<ul class="tablist-3 hide-if-no-js" role="tablist">
 					<li id="tab-c-1" aria-controls="panel-c-1" aria-selected="true" role="tab" tabindex="0">Tab 1</li>
@@ -77,24 +77,24 @@
 
 				<div class="panels">
 					<div id="panel-c-1" role="tabpanel" aria-labelledby="tab-c-1" aria-hidden="false">
-						<h4 class="hide-if-js">Tab 1 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 1 title</h4>
 						<p>First panel contents...</p>
 					</div>
 					<div id="panel-c-2" role="tabpanel" aria-labelledby="tab-c-2" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 2 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 2 title</h4>
 						<p>Second panel contents...</p>
 					</div>
 					<div id="panel-c-3" role="tabpanel" aria-labelledby="tab-c-3" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 3 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 3 title</h4>
 						<p>Third panel contents...</p>
 					</div>
 				</div>
 
 			</div>
 
-			<div class="tabs" id="tabs-d">
+			<div class="tabs tabs-expand-for-mobile" id="tabs-d">
 
-				<h3 class="hide-if-js">Quadruple tab example</h3>
+				<h3 class="tabs-heading hide-if-js">Quadruple tab example</h3>
 
 				<ul class="tablist-4 hide-if-no-js" role="tablist">
 					<li id="tab-d-1" aria-controls="panel-d-1" aria-selected="true" role="tab" tabindex="0">Tab 1</li>
@@ -105,19 +105,19 @@
 
 				<div class="panels">
 					<div id="panel-d-1" role="tabpanel" aria-labelledby="tab-d-1" aria-hidden="false">
-						<h4 class="hide-if-js">Tab 1 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 1 title</h4>
 						<p>First panel contents...</p>
 					</div>
 					<div id="panel-d-2" role="tabpanel" aria-labelledby="tab-d-2" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 2 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 2 title</h4>
 						<p>Second panel contents...</p>
 					</div>
 					<div id="panel-d-3" role="tabpanel" aria-labelledby="tab-d-3" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 3 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 3 title</h4>
 						<p>Third panel contents...</p>
 					</div>
 					<div id="panel-d-4" role="tabpanel" aria-labelledby="tab-d-4" aria-hidden="true">
-						<h4 class="hide-if-js">Tab 4 title</h4>
+						<h4 class="panel-heading hide-if-js">Tab 4 title</h4>
 						<p>Fourth panel contents...</p>
 					</div>
 				</div>
