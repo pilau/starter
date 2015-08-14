9c82594ca599d8e976c19ed8637b9d9fe744bec7
Steve Taylor <steve@sltaylor.co.uk>
2015-08-14 22:52:46 +0100

    Smooth scroll to fragment

--- src/wp-content/themes/pilau-starter/js/global.js
+++ src/wp-content/themes/pilau-starter/js/global.js
@@ -338,20 +338,18 @@ jQuery( document ).ready( function( $ ) {
 	 *
 	 * @link	https://css-tricks.com/snippets/jquery/smooth-scrolling/
 	 */
-	$(function() {
-		$('a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top
-					}, 700);
-					return false;
-				}
+	pilau_body.on( 'click', 'a[href*=#]:not([href=#])', function() {
+		if ( location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname ) {
+			var target = $( this.hash );
+			target = target.length ? target : $( '[name=' + this.hash.slice(1) + ']' );
+			if ( target.length ) {
+				$( 'html,body' ).animate( {
+					scrollTop: target.offset().top
+				}, 700 );
+				return false;
 			}
-		});
-	});
+		}
+	})
 
 
 });
