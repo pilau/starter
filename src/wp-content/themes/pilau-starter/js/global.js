b501ba3abd87441dcb9a2492c01c891fda1fc73a
Steve Taylor <steve@sltaylor.co.uk>
2015-08-14 22:49:13 +0100

    Smooth scroll to fragment

--- src/wp-content/themes/pilau-starter/js/global.js
+++ src/wp-content/themes/pilau-starter/js/global.js
@@ -333,6 +333,27 @@ jQuery( document ).ready( function( $ ) {
 	}
 
 
+	/**
+	 * Smooth scroll to fragment
+	 *
+	 * @link	https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	 */
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 700);
+					return false;
+				}
+			}
+		});
+	});
+
+
 });
 
 
