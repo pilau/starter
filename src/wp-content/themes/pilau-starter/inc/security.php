9b50d109a903377d9361b49ac1c263e5e1f00a62
Steve Taylor <steve@sltaylor.co.uk>
2015-10-13 15:28:49 +0100

    Local dev exempted from front end blocking!

--- src/wp-content/themes/pilau-starter/inc/security.php
+++ src/wp-content/themes/pilau-starter/inc/security.php
@@ -15,7 +15,7 @@ add_action( 'init', 'pilau_block_front_end' );
  * @since	0.1
  */
 function pilau_block_front_end() {
-	if ( defined( 'PILAU_BLOCK_FRONT_END' ) && PILAU_BLOCK_FRONT_END && ! is_admin() && ! pilau_is_login_page() && ! current_user_can( 'update_core' ) ) {
+	if ( ! ( defined( 'WP_LOCAL_DEV' ) && WP_LOCAL_DEV ) && defined( 'PILAU_BLOCK_FRONT_END' ) && PILAU_BLOCK_FRONT_END && ! is_admin() && ! pilau_is_login_page() && ! current_user_can( 'update_core' ) ) {
 		wp_die(
 			'<p>' . __( 'Sorry, the front end of this site is currently blocked.' ) . '</p>',
 			__( 'Front end blocked' ),
