6f1cf5348c93e0aab0c5e69693f2c67fb5e5f1f4
Steve Taylor <steve@sltaylor.co.uk>
2016-01-12 17:43:25 +0000

    SSL check for new SSL filter

--- src/wp-content/themes/pilau-starter/inc/security.php
+++ src/wp-content/themes/pilau-starter/inc/security.php
@@ -164,8 +164,10 @@ add_filter( 'wp_calculate_image_srcset', 'pilau_ssl_srcset' );
  * @link	https://wordpress.org/support/topic/responsive-images-src-url-is-https-srcset-url-is-http-no-images-loaded
  */
 function pilau_ssl_srcset( $sources ) {
-	foreach ( $sources as &$source ) {
-		$source['url'] = set_url_scheme( $source['url'], 'https' );
+	if ( is_ssl() ) {
+		foreach ( $sources as &$source ) {
+			$source['url'] = set_url_scheme( $source['url'], 'https' );
+		}
 	}
 	return $sources;
 }
